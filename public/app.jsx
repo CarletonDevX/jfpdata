2d5ac9ec46753adf4f307ff8567e06160ad802e7
David Pickart <davidstoreypickart@gmail.com>
2016-09-07 21:37:36 -0500

    Get rid of console

--- public/app.jsx
+++ public/app.jsx
@@ -10,15 +10,14 @@ const app = feathers()
 
 const MainComponent = React.createClass({
   getInitialState() {
-    app.service('jobs').find({}).then(res => this.setState({ jobs: res.data }));
-    return {jobs: [], hideSuccessful: false};
-  },
-  addJob(job) {
-    app.service('jobs').create(job).then(job => {
-      var jobs = this.state.jobs;
-      jobs.push(job);
-      this.setState({ jobs: jobs });
+    app.service('jobs').find({}).then(res => {
+      var jobs = res.data;
+      jobs.sort(function(a,b) {
+        return new Date(b.createdAt) - new Date(a.createdAt);
+      });
+      this.setState({ jobs: res.data });
     });
+    return {jobs: [], hideSuccessful: false};
   },
   toggleSuccessful(e) {
     this.setState({ hideSuccessful: (!this.state.hideSuccessful) });
@@ -29,7 +28,6 @@ const MainComponent = React.createClass({
         <h1 className="title"> JFP Data </h1>
         <JobTable jobs={this.state.jobs} hideSuccessful={this.state.hideSuccessful}/>
         <button type="button" className="pure-button" onClick={this.toggleSuccessful}> {this.state.hideSuccessful? "Show Successful Requests": "Hide Successful Requests"} </button>
-        <PrinterConsole onSubmit={this.addJob}/>
       </div>
     );
   }
@@ -66,41 +64,6 @@ const JobTable = React.createClass({
           </tbody>
         </table>
       </div>
-    );
-  }
-});
-
-const PrinterConsole = React.createClass({
-  getInitialState() {
-    return { printer: 'printer', success: 0, copies: 0 };
-  },
-  updatePrinter(ev) {
-    this.setState({ printer: ev.target.value });
-  },
-  updateSuccess(ev) {
-    this.setState({ success: Number(ev.target.value) });
-  },
-  updateCopies(ev) {
-    this.setState({ copies: Number(ev.target.value) });
-  },
-  submit(ev) {
-    this.props.onSubmit(this.state);
-    ev.preventDefault();
-  },
-  render() {
-    return (
-      <form className="pure-form pure-form-stacked" onSubmit={this.submit}>
-        <fieldset>
-          <legend>Enter A Job</legend>
-          <label for="printer">Printer</label>
-          <input type="text" name="printer" className="flex flex-1" value={this.state.printer} onChange={this.updatePrinter} />
-          <label for="success">Successful</label>
-          <input type="number" name="success" className="flex flex-1" value={this.state.success} onChange={this.updateSuccess} />
-          <label for="copies">Copies</label>
-          <input type="number" name="copies" className="flex flex-1" value={this.state.copies} onChange={this.updateCopies} />
-          <button className="button-primary pure-button" type="submit">Send</button>
-        </fieldset>
-      </form>
     );
   }
 });
